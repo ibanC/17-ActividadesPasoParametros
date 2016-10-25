f8649707d36fe455a19c3caec82a20cc4e44642c
ibanC <iban@yopmail.com>
2016-10-25 22:32:10 +0200

    Cambios

--- app/src/main/java/com/example/dm2/a17_actividadespasoparametros/Ejercicio4.java
+++ app/src/main/java/com/example/dm2/a17_actividadespasoparametros/Ejercicio4.java
@@ -3,6 +3,8 @@ package com.example.dm2.a17_actividadespasoparametros;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -13,6 +15,11 @@ public class Ejercicio4 extends AppCompatActivity {
     private EditText txtNombre,txtApellidos;
     private RadioButton rbMasculino,rbFemenino;
     private CheckBox chkMusica,chkLectura,chkDeportes,chkViajar;
+    private Button butEnviar;
+    private String aficiones;
+    private String nombre;
+    private String apellidos;
+    private String sexo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +34,29 @@ public class Ejercicio4 extends AppCompatActivity {
         chkLectura=(CheckBox)findViewById(R.id.chklectura);
         chkDeportes=(CheckBox)findViewById(R.id.chkdeportes);
         chkViajar=(CheckBox)findViewById(R.id.chkviajar);
+        butEnviar=(Button)findViewById(R.id.butEnviar) ;
 
-        String nombre=txtNombre.getText().toString();
-        String apellidos=txtApellidos.getText().toString();
-        String sexo="";
+
+        nombre = txtNombre.getText().toString();
+        if(nombre.equals(""))
+        {
+            Toast.makeText(getApplicationContext(),"Debes introducir tu nombre",Toast.LENGTH_LONG).show();
+        }
+        apellidos = txtApellidos.getText().toString();
+        if(apellidos.equals(""))
+        {
+            Toast.makeText(getApplicationContext(),"Debes introducir tus apellidos",Toast.LENGTH_LONG).show();
+        }
+        sexo = "";
         if(rbMasculino.isChecked())
         {
-            sexo=rbMasculino.getText().toString();
+            sexo =rbMasculino.getText().toString();
         }
         else
         {
             if(rbFemenino.isChecked())
             {
-                sexo=rbFemenino.getText().toString();
+                sexo =rbFemenino.getText().toString();
             }
             else
             {
@@ -47,32 +64,44 @@ public class Ejercicio4 extends AppCompatActivity {
             }
         }
 
-        String aficiones="";
+        aficiones = "";
        if (chkMusica.isChecked())
        {
-           aficiones+=chkMusica.getText().toString();
+           aficiones +=chkMusica.getText().toString();
        }
         else
        {
            if (chkLectura.isChecked())
            {
-               aficiones+=chkLectura.getText().toString();
+               aficiones +=chkLectura.getText().toString();
            }
            else
            {
                if (chkDeportes.isChecked())
                {
-                   aficiones+=chkDeportes.getText().toString();
+                   aficiones +=chkDeportes.getText().toString();
                }
                else
                {
                    if (chkViajar.isChecked())
                    {
-                       aficiones+=chkViajar.getText().toString();
+                       aficiones +=chkViajar.getText().toString();
                    }
                }
            }
        }
+
+        butEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Ejercicio4.this,Ejercicio4Resultado.class);
+                intent.putExtra("nom", nombre);
+                intent.putExtra("ape", apellidos);
+                intent.putExtra("sexo", sexo);
+                intent.putExtra("aficiones", aficiones);
+                startActivity(intent);
+            }
+        });
 
 
     }
---
+++ app/src/main/java/com/example/dm2/a17_actividadespasoparametros/Ejercicio4Resultado.java
@@ -0,0 +1,37 @@
+package com.example.dm2.a17_actividadespasoparametros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Ejercicio4Resultado extends AppCompatActivity {
+
+    private TextView lblnombre,lblapellidos,lblsexo,lblaficiones;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio4_resultado);
+
+        lblnombre=(TextView)findViewById(R.id.textonombre);
+        lblapellidos=(TextView)findViewById(R.id.textoapellido);
+        lblsexo=(TextView)findViewById(R.id.sex) ;
+        lblaficiones=(TextView)findViewById(R.id.aficiones);
+
+        Bundle extras=getIntent().getExtras();
+        String nombre=extras.getString("nom");
+        String apellidos=extras.getString("ape");
+        String sexo=extras.getString("sexo");
+        String aficiones=extras.getString("aficiones");
+
+        if(aficiones.equals(""))
+        {
+            aficiones="No tienes ninguna aficion de las disponibles";
+        }
+
+        lblnombre.setText(lblnombre.getText()+" "+nombre);
+        lblapellidos.setText(lblapellidos.getText()+" "+apellidos);
+        lblsexo.setText(lblsexo.getText()+" "+sexo);
+        lblaficiones.setText(lblaficiones.getText()+" "+aficiones);
+
+    }
+}
